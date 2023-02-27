d83175705f9677cc13bb63f27082fad4c6a58877
Vaše Jméno <vas@email.com>
2023-02-27 15:50:56 +0100

    úkol-2

---
+++ individual-work/úkol-2/src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <p>
+      Omlouvám se, ale zapomněl jsem na úkol.
+    </p> <br>
+    <button type="button" @click="count++">Kolikrát {{ count + 1 }}</button>
+  </div>
+
+  <div class="image">
+    <img src="../assets/crying.jpg"/>
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
